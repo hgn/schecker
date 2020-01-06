337bb4dc917048a7e880b260b1e40caf3184e2a7
Hagen Paul Pfeifer <hagen@jauu.net>
2020-01-06 18:31:18 +0100

    schecker: add spatch functionality
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- schecker/schecker.py
+++ schecker/schecker.py
@@ -21,7 +21,7 @@ from typing import Optional
 class CoccinCheckError(Exception): pass
 class InitializationCocci(CoccinCheckError): pass
 
-COCCIN_SUFFIX = ['.c', '.cpp']
+COCCIN_SUFFIX = ['.c', '.cpp', '.cc']
 
 TIDY_CHECKS  = '-checks=-\*'
 TIDY_CHECKS += ',clang-analyzer-\*'
@@ -34,62 +34,106 @@ TIDY_CHECKS += ',llvm-\*'
 TIDY_CHECKS += ',misc-\*'
 TIDY_CHECKS += ',performance-\*'
 TIDY_CHECKS += ',readability-\*'
-TIDY_CHECKS += ',-google-readability-namespace-comments'
-TIDY_CHECKS += ',-clang-diagnostic-error'
-TIDY_CHECKS += ',-misc-unused-parameters'
-TIDY_CHECKS += ',-llvm-namespace-comment'
-TIDY_CHECKS += ',-google-runtime-references'
 TIDY_CHECKS += ',-clang-diagnostic-error'
 
 INFO_PATH = 'checker-warnings'
 
-class Coccincheck:
+class ModClangTidy:
 
-    def __init__(self, directories: List[str]) -> None:
-        self._directories = directories
+    def __init__(self):
         self._check_environment()
-        self._init_directories()
 
     def _check_environment(self):
         if not shutil.which('clang-tidy'):
             raise CoccinCheckError('clang-tidy not installed or callable, please install clang-tidy')
 
+    def execute(self, relpath, fullpath):
+        cmd =  'clang-tidy {} {}'.format(TIDY_CHECKS, fullpath)
+        stdout = execute(cmd)
+        if len(stdout) >= 0:
+            return "# {}\n{}".format(cmd, stdout)
+        return ''
+
+class ModCoccinelle:
+
+    def __init__(self, coccinelle_script_dirs: List[str] = list()):
+        self._coccinelle_script_dirs = coccinelle_script_dirs
+        self._load_scripts()
+        self._check_environment()
+
+    def _load_scripts(self):
+        self._scripts = list()
+        for rootdir in self._coccinelle_script_dirs:
+            for path, dirs, files in os.walk(rootdir):
+                for file_ in files:
+                    full_path = os.path.join(path, file_)
+                    if not full_path.endswith('.cocci'):
+                        continue
+                    full_path = os.path.normpath(os.path.abspath(full_path))
+                    self._scripts.append(full_path)
+
+
+    def _check_environment(self):
+        if not shutil.which('spatch'):
+            raise CoccinCheckError('spatch not installed or callable, please install clang-tidy')
+
+    def execute(self, relpath, fullpath):
+        buf = ''
+        for scriptpath in self._scripts:
+            cmd =  'spatch -sp_file {} {}'.format(scriptpath, fullpath)
+            stdout = execute(cmd)
+            if len(stdout) >= 0:
+                buf += "# {}\n{}".format(cmd, stdout)
+        return buf
+
+
+class Schecker:
+
+    def __init__(self, directories: List[str], coccinelle_script_dirs: List[str] = list(),
+                 modules_disabled: List[str] = list()) -> None:
+        self._directories = directories
+        self._coccinelle_script_dirs = coccinelle_script_dirs
+        self._modules_disabled = modules_disabled
+        self._init_modules()
+        self._init_directories()
+
+    def _init_modules(self):
+        self._modules = list()
+        if 'coccinelle' not in self._modules_disabled:
+            self._modules.append(ModCoccinelle(self._coccinelle_script_dirs))
+        if 'clang-tidy' not in self._modules_disabled:
+            self._modules.append(ModClangTidy())
+
     def _init_directories(self):
         if not self._directories:
             raise CoccinCheckError('argument for analyzed directories is empty')
-        for directory in self._directories:
-            if not directory.startswith('/'):
-                raise CoccinCheckError('directory argument must be full path')
         if os.path.exists(INFO_PATH):
             shutil.rmtree(INFO_PATH)
 
-    def account_warning(self, rel_path, message):
-        path = os.path.join(INFO_PATH, rel_path)
+    def account_warning(self, relpath, message):
+        path = os.path.join(INFO_PATH, relpath)
         dirpath = os.path.dirname(path)
         os.makedirs(dirpath, exist_ok=True)
-        with open(path, 'w') as fd:
+        with open(path, 'a') as fd:
             fd.write(message)
 
     def check(self):
         buf = io.StringIO()
         for relpath, fullpath in self._each_file():
-            cmd =  'clang-tidy {} {}'.format(TIDY_CHECKS, fullpath)
-            space = ' ' * 128
-            sys.stderr.write('\rprocessing {}{}'.format(relpath, space))
-            stdout = execute(cmd)
-            if len(stdout) <= 0:
-                continue
-            buf.write(stdout)
-            self.account_warning(relpath, stdout)
-        sys.stderr.write('\r')
-        print(buf.getvalue())
-        print('please take a look at {} for file specific warnings'.format(INFO_PATH))
+            for module in self._modules:
+                stdout = module.execute(relpath, fullpath)
+                if len(stdout) <= 0:
+                    continue
+                buf.write(stdout)
+                self.account_warning(relpath, stdout)
+        return buf.getvalue()
 
     def _is_blacklisted(self, path):
         return False
 
     def _each_file(self):
         for rootdir in self._directories:
+            rootdir = os.path.normpath(os.path.abspath(rootdir))
             for path, dirs, files in os.walk(rootdir):
                 for file_ in files:
                     full_path = os.path.join(path, file_)
@@ -118,5 +162,7 @@ def execute(command: str, shell: bool = True, cwd: Optional[str] = None,
 
 
 if __name__ == "__main__":
-    #Coccincheck([os.path.abspath('.')]).check()
-    Coccincheck([os.path.abspath('../botan/src/')]).check()
+    path = ['../botan/src/']
+    path = ['.']
+    scripts = ['schecker/tests/cocci-scripts/']
+    Schecker(path, scripts).check()
